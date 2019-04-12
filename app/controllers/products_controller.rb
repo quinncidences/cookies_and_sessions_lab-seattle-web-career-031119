dd5f856cce5f097e252806ea58aac7ab58baaeb1
quinncidences <qrc.inpa@gmail.com>
2019-04-12 15:26:21 -0700

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+
+  def index
+  end
+
+  def add
+    session[:cart] << params[:product]
+    redirect_to '/'
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+end
